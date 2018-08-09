dc1ce48c5c0b5047a32a356f58b31eefda1e87dc
zmjack <jack@zmland.com>
2018-08-09 16:47:44 +0800

    201808091647

--- push.cmd
+++ push.cmd
@@ -3,23 +3,23 @@ nuget push "Dawnx/bin/Release/Dawnx.1.1.20.nupkg" -source nuget.org
 nuget push "Dawnx.Tools/bin/Release/Dawnx.Tools.1.1.20.nupkg" -source nuget.org
 
 ::==== 'AspNetCore ====
-nuget push "'Dawnx.Library/'AspNetCore/Dawnx.AspNetCore/bin/Release/Dawnx.AspNetCore.1.1.20.nupkg" -source nuget.org
-nuget push "'Dawnx.Library/'AspNetCore/Dawnx.AspNetCore.IdentityUtility/bin/Release/Dawnx.AspNetCore.IdentityUtility.1.1.20.nupkg" -source nuget.org
-nuget push "'Dawnx.Library/'AspNetCore/Dawnx.AspNetCore.LiveAccountUtility/bin/Release/Dawnx.AspNetCore.LiveAccountUtility.1.1.20.nupkg" -source nuget.org
+nuget push "'Library/'AspNetCore/Dawnx.AspNetCore/bin/Release/Dawnx.AspNetCore.1.1.20.nupkg" -source nuget.org
+nuget push "'Library/'AspNetCore/Dawnx.AspNetCore.IdentityUtility/bin/Release/Dawnx.AspNetCore.IdentityUtility.1.1.20.nupkg" -source nuget.org
+nuget push "'Library/'AspNetCore/Dawnx.AspNetCore.LiveAccountUtility/bin/Release/Dawnx.AspNetCore.LiveAccountUtility.1.1.20.nupkg" -source nuget.org
 
 ::==== 'Net ====
-nuget push "'Dawnx.Library/'Net/Dawnx.Net/bin/Release/Dawnx.Net.1.1.20.nupkg" -source nuget.org
+nuget push "'Library/'Net/Dawnx.Net/bin/Release/Dawnx.Net.1.1.20.nupkg" -source nuget.org
 
 ::==== 'NPOI ====
-nuget push "'Dawnx.Library/'NPOI/Dawnx.NPOI/bin/Release/Dawnx.NPOI.1.1.20.nupkg" -source nuget.org
+nuget push "'Library/'NPOI/Dawnx.NPOI/bin/Release/Dawnx.NPOI.1.1.20.nupkg" -source nuget.org
 
 ::==== 'Security ====
-nuget push "'Dawnx.Library/'Security/Dawnx.Security/bin/Release/Dawnx.Security.1.1.20.nupkg" -source nuget.org
+nuget push "'Library/'Security/Dawnx.Security/bin/Release/Dawnx.Security.1.1.20.nupkg" -source nuget.org
 
 ::==== Xml ====
-nuget push "'Dawnx.Library/'Xml/Dawnx.Xml/bin/Release/Dawnx.Xml.1.1.20.nupkg" -source nuget.org
+nuget push "'Library/'Xml/Dawnx.Xml/bin/Release/Dawnx.Xml.1.1.20.nupkg" -source nuget.org
 
 ::==== 'Native ====
-::nuget push "'Dawnx.Library/'Native/Dawnx.Win32/Dawnx.PInvoke.1.0.1.nupkg" -source nuget.org
+::nuget push "'Library/'Native/Dawnx.Win32/Dawnx.PInvoke.1.0.1.nupkg" -source nuget.org
 
 pause

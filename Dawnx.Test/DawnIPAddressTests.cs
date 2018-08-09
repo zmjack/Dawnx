3a7bbac596ac4e792e85881dd4082b2f6cac023a
zmjack <jack@zmland.com>
2018-08-09 16:34:00 +0800

    201808091633

--- Dawnx.Test/DawnIPAddressTests.cs
+++ Dawnx.Test/DawnIPAddressTests.cs
@@ -7,16 +7,16 @@ namespace Dawnx.Test
     public class DawnIPAddressTests
     {
         [Fact]
-        public void GetLongValueOf_IPAddress()
+        public void IPUtilityTest()
         {
             Assert.Equal(0x2414188f, IPAddress.Parse("143.24.20.36").ToLong());
             Assert.Equal("143.24.20.36", new IPAddress(0x2414188f).ToString());
 
-            Assert.Equal(0x8f181424, IPUtility.GetLongValue(IPAddress.Parse("143.24.20.36")));
-            Assert.Equal(0x8f181425, IPUtility.GetLongValue(IPAddress.Parse("143.24.20.37")));
+            Assert.Equal(0x8f181424, IPUtility.GetLongString(IPAddress.Parse("143.24.20.36")));
+            Assert.Equal(0x8f181425, IPUtility.GetLongString(IPAddress.Parse("143.24.20.37")));
 
-            Assert.Equal(0x8f181424, IPUtility.GetLongValue(IPAddress.Parse("143.24.20.36")));
-            Assert.Equal("143.24.20.36", IPUtility.CreateFromLongValue(0x8f181424).ToString());
+            Assert.Equal(0x8f181424, IPUtility.GetLongString(IPAddress.Parse("143.24.20.36")));
+            Assert.Equal("143.24.20.36", IPAddress.Parse(0x8f181424.ToString()).ToString());
         }
     }
 }
--- Dawnx/Utilities/IPUtility.cs
+++ Dawnx/Utilities/IPUtility.cs
@@ -7,20 +7,12 @@ namespace Dawnx.Utilities
     public class IPUtility
     {
         /// <summary>
-        /// Converts a <see cref="IPAddress"/>  into a long value.
+        /// Converts a <see cref="IPAddress"/>  into a long IP string.
         /// </summary>
         /// <param name="ip"></param>
         /// <returns></returns>
-        public static long GetLongValue(IPAddress ip)
+        public static long GetLongString(IPAddress ip)
             => BitConverter.ToUInt32(ip.GetAddressBytes().Reverse().ToArray(), 0);
-
-        /// <summary>
-        /// Converts a IP value into a <see cref="IPAddress"/>.
-        /// </summary>
-        /// <param name="ipValue"></param>
-        /// <returns></returns>
-        public static IPAddress CreateFromLongValue(long ipValue)
-            => new IPAddress(BitConverter.GetBytes((uint)ipValue).Reverse().ToArray());
-
+        
     }
 }

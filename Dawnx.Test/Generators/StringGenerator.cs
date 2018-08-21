8519251366082443595b51fb3ff678355d9c1018
zmjack <jack@zmland.com>
2018-08-21 09:09:32 +0800

    201808210909

---
+++ Dawnx.Test/Generators/StringGenerator.cs
@@ -0,0 +1,27 @@
+﻿using Dawnx.Generators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Dawnx.Test.Generators
+{
+    public class StringGeneratorTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            var taken = new List<string>();
+            for (int i = 0; i < 20; i++)
+            {
+                var generator = new StringGenerator("$d$d", 0);
+
+                var items = generator.Take(5, taken.ToArray());
+                taken.AddRange(items);
+            }
+
+            Assert.Equal(Range.Create(100), taken.Select(x => int.Parse(x)).OrderBy(x => x));
+        }
+    }
+}

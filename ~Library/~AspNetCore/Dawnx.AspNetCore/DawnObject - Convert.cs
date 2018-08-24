c1e20fcd7b1f38ae932dc5c80f7a20576b28be5f
zmjack <jack@zmland.com>
2018-08-24 17:50:59 +0800

    201808241749

---
+++ ~Library/~AspNetCore/Dawnx.AspNetCore/DawnObject - Convert.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Text;
+
+namespace Dawnx
+{
+    public static partial class DawnObject
+    {
+        /// <summary>
+        /// Serializes the specified object to a JSON string.
+        /// </summary>
+        /// <param name="this"></param>
+        /// <returns></returns>
+        public static string GetJson(this object @this) => JsonConvert.SerializeObject(@this);
+
+    }
+}
---
+++ ~Library/~AspNetCore/Dawnx.AspNetCore/DawnString - Convert.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Text;
+
+namespace Dawnx
+{
+    public static partial class DawnString
+    {
+        /// <summary>
+        /// Deserializes the JSON to a .NET object.
+        /// </summary>
+        /// <param name="this"></param>
+        /// <returns></returns>
+        public static T GetFromJson<T>(this string @this) => JsonConvert.DeserializeObject<T>(@this);
+
+    }
+}

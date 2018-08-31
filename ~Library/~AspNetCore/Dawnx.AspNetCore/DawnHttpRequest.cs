d1749c939e8d2e054490450a54f0a66713607fc6
zmjack <jack@zmland.com>
2018-08-31 15:55:20 +0800

    201808311555

--- ~Library/~AspNetCore/Dawnx.AspNetCore/DawnHttpRequest.cs
+++ ~Library/~AspNetCore/Dawnx.AspNetCore/DawnHttpRequest.cs
@@ -5,15 +5,62 @@ namespace Dawnx.AspNetCore
 {
     public static class DawnHttpRequest
     {
+        /// <summary>
+        /// Returns $"{@this.PathBase}{@this.Path}".
+        ///     eg. /controller/action
+        /// </summary>
+        /// <param name="this"></param>
+        /// <returns></returns>
         public static string UrlPath(this HttpRequest @this) => $"{@this.PathBase}{@this.Path}";
-        public static string SchemeHost(this HttpRequest @this) => $"{@this.Scheme}://{@this.Host}";
-        public static string Url(this HttpRequest @this) => $"{SchemeHost(@this)}{UrlPath(@this)}{@this.QueryString}";
 
+        /// <summary>
+        /// Returns $"{@this.PathBase}{@this.Path}{@this.QueryString}".
+        ///     eg. /controller/action?id=1
+        /// </summary>
+        /// <param name="this"></param>
+        /// <returns></returns>
+        public static string FullPath(this HttpRequest @this) => $"{@this.PathBase}{@this.Path}{@this.QueryString}";
+
+        /// <summary>
+        /// Returns $"{@this.Scheme}://{@this.Host}".
+        ///     eg. http://dawnx.net
+        /// </summary>
+        /// <param name="this"></param>
+        /// <returns></returns>
+        public static string SchemeHost(this HttpRequest @this) => $"{@this.Scheme}://{@this.Host}";
+
+        /// <summary>
+        /// Returns $"{@this.Scheme}://{@this.Host}{@this.PathBase}{@this.Path}{@this.QueryString}".
+        ///     eg. http://dawnx.net/controller/action?id=1
+        /// </summary>
+        /// <param name="this"></param>
+        /// <returns></returns>
+        public static string Url(this HttpRequest @this)
+            => $"{@this.Scheme}://{@this.Host}{@this.PathBase}{@this.Path}{@this.QueryString}";
+
+        /// <summary>
+        /// Determines whether the access device is a mobile.
+        /// </summary>
+        /// <param name="this"></param>
+        /// <returns></returns>
         public static bool IsMobile(this HttpRequest @this)
         {
             var userAgent = @this.Headers["User-Agent"].ToString();
             var mobileKeywords = new[] { "Android", "iPhone", "iPod", "iPad", "Windows Phone", "Mobile" };
             return mobileKeywords.Any(x => userAgent.Contains(x));
         }
+
+        /// <summary>
+        /// Determines whether the access device is a tablet.
+        /// </summary>
+        /// <param name="this"></param>
+        /// <returns></returns>
+        public static bool IsTablet(this HttpRequest @this)
+        {
+            var userAgent = @this.Headers["User-Agent"].ToString();
+            var mobileKeywords = new[] { "Android", "iPhone", "iPod", "iPad", "Windows Phone", "Mobile" };
+            return mobileKeywords.Any(x => userAgent.Contains(x));
+        }
+
     }
 }

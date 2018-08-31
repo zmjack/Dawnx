bfe1844efffbbd9602802e4156c1c7e10bf3cbfc
zmjack <jack@zmland.com>
2018-08-31 15:31:54 +0800

    201808311531

--- ~Library/~AspNetCore/Dawnx.AspNetCore/DawnHttpRequest.cs
+++ ~Library/~AspNetCore/Dawnx.AspNetCore/DawnHttpRequest.cs
@@ -5,9 +5,10 @@ namespace Dawnx.AspNetCore
 {
     public static class DawnHttpRequest
     {
-        public static string FullPath(this HttpRequest @this) => $"{@this.PathBase}{@this.Path}{@this.QueryString}";
+        public static string UrlPath(this HttpRequest @this) => $"{@this.PathBase}{@this.Path}{@this.QueryString}";
+        public static string UrlPathWithoutQueryString(this HttpRequest @this) => $"{@this.PathBase}{@this.Path}";
         public static string SchemeHost(this HttpRequest @this) => $"{@this.Scheme}://{@this.Host}";
-        public static string Url(this HttpRequest @this) => $"{SchemeHost(@this)}{FullPath(@this)}";
+        public static string Url(this HttpRequest @this) => $"{SchemeHost(@this)}{UrlPath(@this)}";
 
         public static bool IsMobile(this HttpRequest @this)
         {

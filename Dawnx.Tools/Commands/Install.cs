ac40e2c9e960bc6ecd6ff7619eaec63205d63d8d
zmjack <jack@zmland.com>
2018-10-23 14:57:29 +0800

    201810231457

--- Dawnx.Tools/Commands/Install.cs
+++ Dawnx.Tools/Commands/Install.cs
@@ -18,136 +18,145 @@ namespace Dawnx.Tools
                 ["name"] = name,
             });
 
-            var resp = JsonConvert.DeserializeObject<SimpleResponse>(respJson);
-            if (resp.Success)
+            try
             {
-                var model = resp.model as JObject;
-                var cli_version = model["cli_version"].Value<string>();
-
-                if (new Version(Program.CLI_VERSION) >= new Version(cli_version))
+                var resp = JsonConvert.DeserializeObject<SimpleResponse>(respJson);
+                if (resp.Success)
                 {
-                    int fileCount = model["count"].Value<int>();
-                    int fileDownload = 0;
-                    int fileSkip = 0;
-                    int fileDone() => fileDownload + fileSkip;
-                    int fileVerifySuccess = 0;
-                    int fileVerifyFailed = 0;
+                    var model = resp.model as JObject;
+                    var cli_version = model["cli_version"].Value<string>();
 
-                    int totalProgressFiledLength = fileCount * 2 + 1;
-                    int filenameFiledLength = 67 - totalProgressFiledLength;
-
-                    List<string> extractFileList = new List<string>();
-
-                    foreach (var item in model["files"] as JArray)
+                    if (new Version(Program.CLI_VERSION) >= new Version(cli_version))
                     {
-                        var url = item["url"].Value<string>();
-                        var md5 = item["md5"].Value<string>();
-                        var saveas = $@"{Program.DOWNLOAD_DIRECTORY}\{Path.GetFileName(url)}";
-                        var extract = item["extract"].Value<bool>();
+                        int fileCount = model["count"].Value<int>();
+                        int fileDownload = 0;
+                        int fileSkip = 0;
+                        int fileDone() => fileDownload + fileSkip;
+                        int fileVerifySuccess = 0;
+                        int fileVerifyFailed = 0;
 
-                        Console.Write(
-                            $"{$"{fileDone() + 1}/{fileCount}".PadLeft(totalProgressFiledLength)} " +
-                            $"| " + Path.GetFileName(saveas)
-                                .For(_ => _.Length <= filenameFiledLength ? _ : _.Substring(0, filenameFiledLength))
-                                .PadRight(filenameFiledLength) +
-                            $"|");
+                        int totalProgressFiledLength = fileCount * 2 + 1;
+                        int filenameFiledLength = 67 - totalProgressFiledLength;
 
-                        if (!File.Exists(saveas) || !FileUtility.CheckMD5(saveas, md5))
+                        List<string> extractFileList = new List<string>();
+
+                        foreach (var item in model["files"] as JArray)
                         {
-                            #region Download files
-                            using (var file = new FileStream(saveas, FileMode.Create))
+                            var url = item["url"].Value<string>();
+                            var md5 = item["md5"].Value<string>();
+                            var saveas = $@"{Program.DOWNLOAD_DIRECTORY}\{Path.GetFileName(url)}";
+                            var extract = item["extract"].Value<bool>();
+
+                            Console.Write(
+                                $"{$"{fileDone() + 1}/{fileCount}".PadLeft(totalProgressFiledLength)} " +
+                                $"| " + Path.GetFileName(saveas)
+                                    .For(_ => _.Length <= filenameFiledLength ? _ : _.Substring(0, filenameFiledLength))
+                                    .PadRight(filenameFiledLength) +
+                                $"|");
+
+                            if (!File.Exists(saveas) || !FileUtility.CheckMD5(saveas, md5))
                             {
-                                var web = new WebAccess();
-                                web.DownloadProgress += (sender, _url, received, length) =>
+                                #region Download files
+                                using (var file = new FileStream(saveas, FileMode.Create))
                                 {
-                                    Console.SetCursorPosition(72, Console.CursorTop);
-                                    Console.Write($"{((double)received / length).ToString("0.00%").PadLeft(7)}");
-                                };
-
-                                int retry = 0, allowedRetry = 3;
-                                retry:
-                                try
-                                {
-                                    web.GetDownload(file, url);
-
-                                    if (extract)
-                                        extractFileList.Add(saveas);
-                                    fileDownload++;
-                                }
-                                catch (Exception ex)
-                                {
-                                    Console.WriteLine();
-                                    if (retry < allowedRetry)
+                                    var web = new WebAccess();
+                                    web.DownloadProgress += (sender, _url, received, length) =>
                                     {
-                                        Console.WriteLine($"  {ex.Message}, retry {++retry}/{allowedRetry}");
-                                        goto retry;
+                                        Console.SetCursorPosition(72, Console.CursorTop);
+                                        Console.Write($"{((double)received / length).ToString("0.00%").PadLeft(7)}");
+                                    };
+
+                                    int retry = 0, allowedRetry = 3;
+                                    retry:
+                                    try
+                                    {
+                                        web.GetDownload(file, url);
+
+                                        if (extract)
+                                            extractFileList.Add(saveas);
+                                        fileDownload++;
                                     }
-                                    else
+                                    catch (Exception ex)
                                     {
-                                        Console.WriteLine($"  File can not be downloaded from {url}");
-                                        if (AlertUtility.AskYN("Retry?"))
+                                        Console.WriteLine();
+                                        if (retry < allowedRetry)
                                         {
-                                            retry = 0;
+                                            Console.WriteLine($"  {ex.Message}, retry {++retry}/{allowedRetry}");
                                             goto retry;
                                         }
                                         else
                                         {
-                                            fileSkip++;
-                                            continue;
+                                            Console.WriteLine($"  File can not be downloaded from {url}");
+                                            if (AlertUtility.AskYN("Retry?"))
+                                            {
+                                                retry = 0;
+                                                goto retry;
+                                            }
+                                            else
+                                            {
+                                                fileSkip++;
+                                                continue;
+                                            }
                                         }
                                     }
                                 }
-                            }
-                            #endregion
+                                #endregion
 
-                            #region Check file md5
-                            Console.SetCursorPosition(72, Console.CursorTop);
-                            if (FileUtility.CheckMD5(saveas, md5))
-                            {
-                                fileVerifySuccess++;
-                                Console.WriteLine(" Safe  ");
+                                #region Check file md5
+                                Console.SetCursorPosition(72, Console.CursorTop);
+                                if (FileUtility.CheckMD5(saveas, md5))
+                                {
+                                    fileVerifySuccess++;
+                                    Console.WriteLine(" Safe  ");
+                                }
+                                else
+                                {
+                                    fileVerifyFailed++;
+                                    Console.WriteLine("WARNING");
+                                }
+                                #endregion
                             }
                             else
                             {
-                                fileVerifyFailed++;
-                                Console.WriteLine("WARNING");
+                                if (extract)
+                                    extractFileList.Add(saveas);
+                                fileDownload++;
+
+                                Console.SetCursorPosition(72, Console.CursorTop);
+                                Console.WriteLine(" Found ");
                             }
-                            #endregion
                         }
-                        else
-                        {
-                            if (extract)
-                                extractFileList.Add(saveas);
-                            fileDownload++;
 
-                            Console.SetCursorPosition(72, Console.CursorTop);
-                            Console.WriteLine(" Found ");
-                        }
-                    }
-
-                    Console.WriteLine();
-                    Console.WriteLine(
-                        $"Result: {fileDownload} downloaded, " +
-                        $"{fileVerifySuccess} safe, " +
-                        $"{fileVerifyFailed} warning. " +
-                        $"{fileSkip} skiped.");
-                    Console.WriteLine($"---- All files has been downloaded using engine Dawnx.Net.Http.Web ----{Environment.NewLine}");
-
-                    // Setup
-                    if (AlertUtility.AskYN("Setup now?"))
-                    {
                         Console.WriteLine();
-                        foreach (var file in extractFileList)
+                        Console.WriteLine(
+                            $"Result: {fileDownload} downloaded, " +
+                            $"{fileVerifySuccess} safe, " +
+                            $"{fileVerifyFailed} warning. " +
+                            $"{fileSkip} skiped.");
+                        Console.WriteLine($"---- All files has been downloaded using engine Dawnx.Net.Http.Web ----{Environment.NewLine}");
+
+                        // Setup
+                        if (AlertUtility.AskYN("Setup now?"))
                         {
-                            ZipFile.ExtractToDirectory(file, Directory.GetCurrentDirectory(), true);
-                            Console.WriteLine($"Extract {file} done.");
+                            Console.WriteLine();
+                            foreach (var file in extractFileList)
+                            {
+                                ZipFile.ExtractToDirectory(file, Directory.GetCurrentDirectory(), true);
+                                Console.WriteLine($"Extract {file} done.");
+                            }
+                            Console.WriteLine($"---- Extract files completed ----{Environment.NewLine}");
                         }
-                        Console.WriteLine($"---- Extract files completed ----{Environment.NewLine}");
                     }
+                    else Console.WriteLine($"Install service requires the lowest cli tool version: {cli_version}.");
                 }
-                else Console.WriteLine($"Install service requires the lowest cli tool version: {cli_version}.");
+                else AlertUtility.PrintErrorMessage(resp);
+
             }
-            else AlertUtility.PrintErrorMessage(resp);
+            catch (JsonReaderException ex)
+            {
+                Console.WriteLine("Error occurred, the server response is:");
+                Console.WriteLine(respJson);
+            }
         }
 
     }

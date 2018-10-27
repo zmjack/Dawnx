02700fb79aef03d83528964059d17297cbac8dfa
zmjack <jack@zmland.com>
2018-10-28 04:29:30 +0800

    201810280429

--- ~Library/~AspNetCore/Dawnx.AspNetCore/Entity/EntityMonitor.cs
+++ ~Library/~AspNetCore/Dawnx.AspNetCore/Entity/EntityMonitor.cs
@@ -14,25 +14,12 @@ namespace Dawnx.AspNetCore.Entity
         public static Dictionary<string, object> ModifyMonitors { get; private set; } = new Dictionary<string, object>();
         public static Dictionary<string, object> DeleteMonitors { get; private set; } = new Dictionary<string, object>();
 
-        public static void Register<TEntity>(EntityState type, MonitorInvoker invoker)
-        {
-            switch (type)
-            {
-                case EntityState.Added:
-                    AddMonitors[typeof(TEntity).FullName] = invoker;
-                    break;
-
-                case EntityState.Modified:
-                    ModifyMonitors[typeof(TEntity).FullName] = invoker;
-                    break;
-
-                case EntityState.Deleted:
-                    DeleteMonitors[typeof(TEntity).FullName] = invoker;
-                    break;
-
-                default: throw new NotSupportedException("Only Added, Modified, Deleted are supported.");
-            }
-        }
+        public static void RegisterForAdded<TEntity>(MonitorInvoker invoker)
+            => AddMonitors[typeof(TEntity).FullName] = invoker;
+        public static void RegisterForModified<TEntity>(MonitorInvoker invoker)
+            => ModifyMonitors[typeof(TEntity).FullName] = invoker;
+        public static void RegisterForDeleted<TEntity>(MonitorInvoker invoker)
+            => DeleteMonitors[typeof(TEntity).FullName] = invoker;
 
         public static MonitorInvoker GetMonitor<TEntity>(EntityState type)
             => GetMonitor(typeof(TEntity).FullName, type);

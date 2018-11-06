756a56b9607f1dc039ca13bcf517fb85821e6e69
zmjack <jack@zmland.com>
2018-11-06 10:31:31 +0800

    201811061031

---
+++ ~Library/~AspNetCore/Dawnx.AspNetCore/DawnViewHtmlHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dawnx.AspNetCore
+{
+    public static class DawnViewHtmlHelper
+    {
+        public static HtmlString JValidate<TModel>(this HtmlHelper @this, VI<TModel> model)
+        {
+            return new HtmlString("");
+        }
+
+    }
+}
--- ~Library/~AspNetCore/Dawnx.AspNetCore/~Std/JValidate.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Dawnx.AspNetCore
-{
-    public static class DawnViewHtmlHelper
-    {
-        //public static JValidate(this Microsoft.AspNetCore.Mvc.ViewFeatures.HtmlHelper @this)
-    }
-}

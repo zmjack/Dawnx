a66336ee9eb4592e026e36fecb950d46e6b728d4
zmjack <jack@zmland.com>
2018-11-06 16:31:56 +0800

    201811061631

---
+++ Dawnx/DawnEnumerator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dawnx
+{
+    public static class DawnEnumerator
+    {
+        public static TElement TakeElement<TElement>(this IEnumerator<TElement> @this)
+        {
+            if (@this.MoveNext())
+                return @this.Current;
+            else return default(TElement);
+        }
+
+        public static object TakeElement(this IEnumerator @this)
+        {
+            if (@this.MoveNext())
+                return @this.Current;
+            else return null;
+        }
+
+    }
+}

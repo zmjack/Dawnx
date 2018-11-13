ebbd5675e9b315c66701bf36c9577c4d2f04f8dc
zmjack <jack@zmland.com>
2018-11-13 17:58:49 +0800

    201811131758

--- Dawnx/Patterns/DoubleCheck.cs
+++ Dawnx/Patterns/DoubleCheck.cs
@@ -3,10 +3,16 @@
 namespace Dawnx.Patterns
 {
     /// <summary>
-    /// Double-checked locking pattern: if -> lock -> if -> dosth.
+    /// Double-checked locking pattern: if(condition) -> lock(locker) -> if(condition) -> do(task).
     /// </summary>
     public static class DoubleCheck
     {
+        /// <summary>
+        /// Do task with Double-checked locking pattern.
+        /// </summary>
+        /// <param name="locker"></param>
+        /// <param name="condition"></param>
+        /// <param name="task"></param>
         public static void Do(string locker, Func<bool> condition, Action task)
         {
             if (condition())
@@ -15,7 +21,14 @@ namespace Dawnx.Patterns
                         task();
         }
 
-        public static void Do<T>(ref T locker, Func<bool> condition, Action task)
+        /// <summary>
+        /// Do task with Double-checked locking pattern.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="locker"></param>
+        /// <param name="condition"></param>
+        /// <param name="task"></param>
+        public static void Do<T>(T locker, Func<bool> condition, Action task)
         {
             if (condition())
                 lock (locker)

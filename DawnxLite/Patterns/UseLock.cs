d1722e50c60b1218790001b92375c0dc84497664
zmjack <jack@zmland.com>
2018-11-22 14:21:54 +0800

    201811221421

---
+++ DawnxLite/Patterns/UseLock.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Dawnx.Patterns
+{
+    public static class UseLock
+    {
+        public static void Do(object lockObj, TimeSpan timeout, Action task)
+        {
+            bool lockTaken = false;
+            try
+            {
+                Monitor.TryEnter(lockObj, timeout, ref lockTaken);
+                task();
+            }
+            finally
+            {
+                if (lockTaken)
+                    Monitor.Exit(lockObj);
+            }
+        }
+
+    }
+}

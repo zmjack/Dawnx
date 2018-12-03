e7ab4693fa5d316fb24b8dbfcdab02cdb37f5219
zmjack <jack@zmland.com>
2018-12-03 22:09:02 +0800

    201812032208

---
+++ DawnxDevloping/Program - TicketingScenario.cs
@@ -0,0 +1,38 @@
+﻿using Dawnx;
+using Dawnx.Diagnostics;
+using Dawnx.Generators;
+using Dawnx.Patterns;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace DawnxDevloping
+{
+    class Program
+    {
+        static string NewConstString => new StringBuilder().Self(_ => _.Append("a")).ToString();
+        static int RemainingTicket = 1_000_000;
+
+        static void Main(string[] args)
+        {
+            Sell(1_000_000);
+            Console.WriteLine(RemainingTicket);
+        }
+
+        static void Sell(int level)
+        {
+            using (var probe = PerformanceProbe.Create(nameof(Sell)))
+            {
+                var ret = Concurrency.Run((runId) =>
+                {
+                    lock (Locker.Get(NewConstString))
+                    {
+                        return RemainingTicket--;
+                    }
+                }, level);
+            }
+        }
+
+    }
+}
---
+++ DawnxLite/~Std/Locker.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Dawnx
+{
+    public static class Locker
+    {
+        /// <summary>
+        /// Craetes a special reference of string from the system.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="strs"></param>
+        /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static string Get<T>(params object[] strs)
+        {
+            return string.Intern($"{typeof(T).FullName}{"\0"}{strs.Join("\0")}");
+        }
+
+        /// <summary>
+        /// Craetes a special reference of string from the system.
+        /// </summary>
+        /// <param name="strs"></param>
+        /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static string Get(params object[] strs)
+        {
+            return string.Intern($"{strs.Join("\0")}");
+        }
+
+    }
+}

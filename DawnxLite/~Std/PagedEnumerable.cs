cb465deebfb4880601b09cfacef1b1bdc8f46a9b
zmjack <jack@zmland.com>
2018-12-05 17:24:35 +0800

    201812051724

--- DawnxLite/~Std/PagedEnumerable.cs
+++ DawnxLite/~Std/PagedEnumerable.cs
@@ -12,14 +12,17 @@ namespace Dawnx
         public int PageCount { get; protected set; }
         public bool IsFristPage => PageNumber == 1;
         public bool IsLastPage => PageNumber == PageCount;
-
-        protected PagedEnumerable() { }
-
+        
         public PagedEnumerable(IEnumerable<T> source, int page, int pageSize)
         {
-            PageNumber = page;
             PageSize = pageSize;
             PageCount = source.PageCount(pageSize);
+            switch (page)
+            {
+                case int p when p < 1: PageNumber = 1; break;
+                case int p when p > PageCount: PageNumber = PageCount; break;
+                default: PageNumber = page; break;
+            }
             Items = source.Skip((PageNumber - 1) * PageSize).Take(PageSize);
         }
 
--- DawnxLite/~Std/PagedQueryable.cs
+++ DawnxLite/~Std/PagedQueryable.cs
@@ -11,12 +11,7 @@ namespace Dawnx
         public IQueryProvider Provider => (Items as IQueryable<T>).Provider;
 
         public PagedQueryable(IQueryable<T> source, int page, int pageSize)
-        {
-            PageNumber = page;
-            PageSize = pageSize;
-            PageCount = source.PageCount(pageSize);
-            Items = source.Skip((PageNumber - 1) * PageSize).Take(PageSize);
-        }
+            : base(source, page, pageSize) { }
 
     }
 

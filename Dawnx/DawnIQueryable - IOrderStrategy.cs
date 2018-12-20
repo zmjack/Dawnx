1d861a416ed9fbefeeced0da9f925814bc8daa66
zmjack <jack@zmland.com>
2018-12-20 18:42:44 +0800

    201812201842

---
+++ Dawnx/DawnIQueryable - IOrderStrategy.cs
@@ -0,0 +1,42 @@
+﻿using Dawnx.Entity;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Dawnx
+{
+    public static partial class DawnIQueryable
+    {
+        /// <summary>
+        /// Use an OrderByStrategy to generate an orberby expression.
+        /// </summary>
+        /// <typeparam name="TEntity"></typeparam>
+        /// <param name="this"></param>
+        /// <param name="strategy"></param>
+        /// <returns></returns>
+        public static IOrderedQueryable<TEntity> OrderByCaseStrategy<TEntity>(this IQueryable<TEntity> @this, IOrderStrategy<TEntity> strategy)
+            => @this.OrderBy(strategy.StrategyExpression);
+
+        public static IOrderedQueryable<TEntity> OrderByCase<TEntity>(this IQueryable<TEntity> @this,
+            Expression<Func<TEntity, string>> memberExp,
+            string[] orderValues)
+            => @this.OrderByCaseStrategy(new OrderByCaseStrategy<TEntity>(memberExp, orderValues));
+
+        /// <summary>
+        /// Use an OrderByStrategy to generate an orberby expression.
+        /// </summary>
+        /// <typeparam name="TEntity"></typeparam>
+        /// <param name="this"></param>
+        /// <param name="strategy"></param>
+        /// <returns></returns>
+        internal static IOrderedQueryable<TEntity> OrderByCaseDescendingStrategy<TEntity>(this IQueryable<TEntity> @this, IOrderStrategy<TEntity> strategy)
+            => @this.OrderByDescending(strategy.StrategyExpression);
+
+        public static IOrderedQueryable<TEntity> OrderByCaseDescending<TEntity>(this IQueryable<TEntity> @this,
+            Expression<Func<TEntity, string>> memberExp,
+            string[] orderValues)
+            => @this.OrderByCaseDescendingStrategy(new OrderByCaseStrategy<TEntity>(memberExp, orderValues));
+        
+    }
+
+}

19dbf284df8253cbed5e357c60ad14e7dd19c621
zmjack <jack@zmland.com>
2018-12-28 00:44:56 +0800

    201812280044

---
+++ Dawnx.Test/~Dawnx/Algorithms/Math/ForecastTest.cs
@@ -0,0 +1,22 @@
+﻿using Dawnx.Algorithms.Math;
+using Dawnx.Ranges;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Dawnx.Test.Algorithms.Math
+{
+    public class ForecastTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            var result = Forecast.Linear(
+                  known_x: new IntegerRange(2010, 2015).Select(x => (double)x).ToArray(),
+                  known_y: new[] { 249_000.00, 300_000.00, 250_000.00, 340_000.00, 540_000.00, 367_000.00 },
+                  forecast_x: 2016);
+            Assert.Equal(481000, result, 0);
+        }
+    }
+}
---
+++ DawnxLite/Algorithms/Math/Forecast.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dawnx.Algorithms.Math
+{
+    public static class Forecast
+    {
+        public static double Linear(double[] known_x, double[] known_y, double forecast_x)
+        {
+            if (known_y.Length != known_x.Length)
+                throw new ArgumentException($"The `{nameof(known_x)}`'s length must be equal to the `{nameof(known_y)}`'s length.");
+
+            var avg_x = known_x.Average();
+            var avg_y = known_y.Average();
+
+            var seq_x = known_x.GetEnumerator();
+
+            var bounds = known_y
+                .Aggregate(new { Top = 0.0, Bottom = 0.0 }, (acc, y) =>
+                {
+                    var x = (double)seq_x.TakeElement();
+                    return new
+                    {
+                        Top = acc.Top + (x - avg_x) * (y - avg_y),
+                        Bottom = acc.Bottom + System.Math.Pow(x - avg_x, 2.0)
+                    };
+                });
+
+            var level = bounds.Top / bounds.Bottom;
+
+            return (avg_y - level * avg_x) + level * forecast_x;
+        }
+    }
+}

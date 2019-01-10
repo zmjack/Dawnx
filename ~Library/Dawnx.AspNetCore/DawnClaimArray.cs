1febd12a209792694b05c7ed2a8bfa2f3d5e44ca
zmjack <jack@zmland.com>
2019-01-10 20:11:32 +0800

    201901102011

---
+++ ~Library/Dawnx.AspNetCore/DawnClaimArray.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+
+namespace Dawnx.AspNetCore
+{
+    public static class DawnClaimArray
+    {
+        /// <summary>
+        /// Gets the value of `sub`. If the claims don't contains it, return null.
+        /// </summary>
+        /// <param name="this"></param>
+        /// <returns></returns>
+        public static string[] GetClaim(this IEnumerable<Claim> @this)
+            => @this.Where(x => x.Type == "sub").Select(x => x.Value).ToArray();
+
+        /// <summary>
+        /// Determines if reqeust is sent from `sub`.
+        /// </summary>
+        /// <param name="this"></param>
+        /// <returns></returns>
+        public static bool IsContainsSubject(this IEnumerable<Claim> @this) => @this.Any(x => x.Type == "sub");
+
+    }
+}

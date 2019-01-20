202d52607bada9d7b0b79386ce9f2aa8911c1adb
zmjack <jack@zmland.com>
2019-01-20 18:39:14 +0800

    201901201839

---
+++ ~Localization/Dawnx.Chinese/Utilities/CurrencyUtility.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Dawnx.Chinese
+{
+    public class CurrencyUtility : Localization.CurrencyUtility
+    {
+        private static CurrencyUtility _Instance = new CurrencyUtility();
+
+        private static readonly char[] Values = new char[] { '零', '壹', '贰', '叁', '肆', '伍', '陆', '柒', '捌', '玖' };
+        private static readonly char[] Units = new char[] { '元', '拾', '佰', '仟', '万', '拾', '佰', '仟', '亿' };
+
+        public static string GetString(double money) => _Instance._GetString(money);
+        protected override string _GetString(double money)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static double GetDouble(string str) => _Instance._GetDouble(str);
+        protected override double _GetDouble(string str)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ ~Localization/Dawnx.Localization/Utilities/CurrencyUtility.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Dawnx.Localization
+{
+    public abstract class CurrencyUtility
+    {
+        protected abstract string _GetString(double money);
+        protected abstract double _GetDouble(string money);
+
+    }
+}

e4e3a838b6da51c527451d1df409485c087e4dc7
zmjack <jack@zmland.com>
2019-01-23 20:23:16 +0800

    201901232023

--- Dawnx.Test/Chinese/CurrencyUtilityTest.cs
+++ Dawnx.Test/Chinese/CurrencyUtilityTest.cs
@@ -5,35 +5,51 @@ namespace Dawnx.Chinese.Test
     public class CurrencyUtilityTest
     {
         [Fact]
-        public void Test1()
+        public void IntegerTest()
         {
-            Assert.Equal("十万〇一", CurrencyUtility.GetString(10_0001, new CurrencyOption
+            Assert.Equal("十万〇一元整", CurrencyUtility.GetString(10_0001, new CurrencyOption
             {
                 IsSimplified = true,
                 Target = CurrencyOption.ETarget.Lower,
             }));
 
-            Assert.Equal("一十万〇一", CurrencyUtility.GetString(10_0001, new CurrencyOption
+            Assert.Equal("一十万〇一元整", CurrencyUtility.GetString(10_0001, new CurrencyOption
             {
                 IsSimplified = false,
                 Target = CurrencyOption.ETarget.Lower,
             }));
 
-            Assert.Equal("一十万〇一百〇一", CurrencyUtility.GetString(10_0101, new CurrencyOption
+            Assert.Equal("一十万〇一百〇一元整", CurrencyUtility.GetString(10_0101, new CurrencyOption
             {
                 IsSimplified = false,
                 Target = CurrencyOption.ETarget.Lower,
             }));
 
-            Assert.Equal("一十万一千〇一", CurrencyUtility.GetString(10_1001, new CurrencyOption
+            Assert.Equal("一十万一千〇一元整", CurrencyUtility.GetString(10_1001, new CurrencyOption
             {
                 IsSimplified = false,
                 Target = CurrencyOption.ETarget.Lower,
             }));
 
-            Assert.Equal("一十万一千〇一十", CurrencyUtility.GetString(10_1010, new CurrencyOption
+            Assert.Equal("一十万一千〇一十元整", CurrencyUtility.GetString(10_1010, new CurrencyOption
             {
                 IsSimplified = false,
+                Target = CurrencyOption.ETarget.Lower,
+            }));
+        }
+
+        [Fact]
+        public void DoubleTest()
+        {
+            Assert.Equal("十万〇一元二角整", CurrencyUtility.GetString(10_0001.2, new CurrencyOption
+            {
+                IsSimplified = true,
+                Target = CurrencyOption.ETarget.Lower,
+            }));
+
+            Assert.Equal("十万〇一元二角三分", CurrencyUtility.GetString(10_0001.23, new CurrencyOption
+            {
+                IsSimplified = true,
                 Target = CurrencyOption.ETarget.Lower,
             }));
         }
--- ~Localization/Dawnx.Chinese/Utilities/CurrencyUtility.cs
+++ ~Localization/Dawnx.Chinese/Utilities/CurrencyUtility.cs
@@ -11,11 +11,13 @@ namespace Dawnx.Chinese
         private static readonly string[] Levels = new string[] { "亿", "万", "" };
         private static readonly string[] LowerSingleUnits = new string[] { "千", "百", "十", "" };
         private static readonly string[] UpperSingleUnits = new string[] { "仟", "佰", "拾", "" };
-        private static readonly string[] Unit = new string[] { "元", "角", "分" };
 
-        public static string GetString(double money, CurrencyOption option) => GetString((int)money, (int)money % 1, option);
-        public static string GetString(int integer, int fractional, CurrencyOption option)
+        public static string GetString(double money, CurrencyOption option) => GetString((int)money, (int)(money * 100 % 100), option);
+        public static string GetString(int integer, int fractional100, CurrencyOption option)
         {
+            if (!(0 <= fractional100 && fractional100 <= 99))
+                throw new ArgumentException($"The argument `{nameof(fractional100)}` must be between 0 and 99.");
+
             string GetPartString(char[] singles, string level)
             {
                 if (!singles.Any()) return string.Empty;
@@ -28,6 +30,7 @@ namespace Dawnx.Chinese
                     case CurrencyOption.ETarget.Upper: zeroValue = UpperValues[0]; break;
                     default: throw new NotSupportedException();
                 }
+
                 var zero = false;
                 foreach (var vi in singles.AsVI())
                 {
@@ -61,14 +64,37 @@ namespace Dawnx.Chinese
             }
 
             var levelParts = integer.ToString().Distribute(4, true).ToArray();
-            var str = levelParts.Select((v, i) => GetPartString(v, Levels[Levels.Length - levelParts.Length + i])).Join("");
+            var ret = levelParts.Select((v, i) => GetPartString(v, Levels[Levels.Length - levelParts.Length + i])).Join("");
 
-            if (option.IsSimplified && (str.StartsWith("一十") || str.StartsWith("壹拾")))
-                return str.Substring(1);
-            else return str;
+            if (option.IsSimplified && (ret.StartsWith("一十") || ret.StartsWith("壹拾")))
+                ret = ret.Substring(1);
+
+            if (fractional100 == 0)
+                return $"{ret}元整";
+            else if (fractional100 % 10 == 0)
+            {
+                switch (option.Target)
+                {
+                    case CurrencyOption.ETarget.Lower: return $"{ret}元{LowerValues[fractional100 / 10]}角整";
+                    case CurrencyOption.ETarget.Upper: return $"{ret}元{UpperValues[fractional100 / 10]}角整";
+                    default: throw new NotSupportedException();
+                }
+            }
+            else
+            {
+                switch (option.Target)
+                {
+                    case CurrencyOption.ETarget.Lower: return $"{ret}元{LowerValues[fractional100 / 10]}角{LowerValues[fractional100 % 10]}分";
+                    case CurrencyOption.ETarget.Upper: return $"{ret}元{UpperValues[fractional100 / 10]}角{UpperValues[fractional100 % 10]}分";
+                    default: throw new NotSupportedException();
+                }
+            }
         }
 
-        //public static double GetDouble(string str) => _Instance._GetDouble(str);
+        public static double GetDouble(string str)
+        {
+            throw new NotImplementedException();
+        }
 
     }
 }

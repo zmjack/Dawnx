60535e4ffd0a96c1fddf1706f3e5abca84236d2b
zmjack <jack@zmland.com>
2019-02-02 03:56:46 +0800

    201902020356

---
+++ DawnxLite/~Std/Linear.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dawnx
+{
+    public class Linear
+    {
+        public static IEnumerable<TRet> Create<T1, T2, TRet>(IEnumerable<T1> list1, IEnumerable<T2> list2, Func<T1, T2, TRet> linear)
+        {
+            var enumerator1 = list1.GetEnumerator();
+            var enumerator2 = list2.GetEnumerator();
+
+            while (true)
+            {
+                var move = new[] { enumerator1.MoveNext(), enumerator2.MoveNext() };
+                if (move.All(x => x))
+                    yield return linear(enumerator1.Current, enumerator2.Current);
+                else break;
+            }
+        }
+    }
+}

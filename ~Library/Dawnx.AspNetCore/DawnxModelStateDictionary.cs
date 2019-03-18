5c44f3cbde7ee540bf4ed987406f93e9955828c8
zmjack <jack@zmland.com>
2019-03-18 16:33:12 +0800

    201903181633

---
+++ ~Library/Dawnx.AspNetCore/DawnxModelStateDictionary.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dawnx.AspNetCore
+{
+    public static class DawnxModelStateDictionary
+    {
+        public static Dictionary<string, string> ErrorMap(this ModelStateDictionary @this)
+        {
+            var errors = @this
+                .Select(x => new KeyValuePair<string, string>(x.Key, x.Value.Errors[0].ErrorMessage))
+                .ToDictionary(x => x.Key, x => x.Value);
+            return errors;
+        }
+    }
+}

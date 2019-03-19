88a54dc5f33d78338d7a8f40aa546666b38806a5
zmjack <jack@zmland.com>
2019-03-19 16:58:27 +0800

    201903191658

---
+++ ~Library/Dawnx.AspNetCore/DawnHttpResponse.cs
@@ -0,0 +1,24 @@
+﻿using Dawnx.Utilities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dawnx.AspNetCore
+{
+    public static class DawnHttpResponse
+    {
+        public static void ClearCookies(this HttpResponse @this, IRequestCookieCollection cookies)
+        {
+            foreach (var key in cookies.Keys)
+            {
+                @this.Cookies.Append(key, "", new CookieOptions
+                {
+                    Expires = DateTimeUtility.UnixMinValue(),
+                });
+            }
+        }
+
+    }
+}

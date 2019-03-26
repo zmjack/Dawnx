34aabd0a6be38865f36acf52c79424df380aad6f
zmjack <jack@zmland.com>
2019-03-26 20:09:56 +0800

    201903262009

---
+++ Dawnx.Tools/TsUtility.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using TypeLite;
+using TypeLite.Net4;
+
+namespace Dawnx.Tools
+{
+    public static class TsUtility
+    {
+        public static void GenerateTsFile(Assembly assembly)
+        {
+            var tsFluent = TypeScript.Definitions().ForReferencedAssembly(assembly.FullName);
+            var name = Assembly.GetExecutingAssembly().GetName().Name;
+            File.WriteAllText($"{name}.d.ts", tsFluent.Generate(
+                TsGeneratorOutput.Enums | TsGeneratorOutput.Properties | TsGeneratorOutput.Fields));
+            File.WriteAllText($"{name}.const.d.ts", tsFluent.Generate(TsGeneratorOutput.Constants));
+        }
+
+    }
+}

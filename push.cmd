c4fbdcbd0233d0f9d697b863ec94cf27cff707b0
zmjack <jack@zmland.com>
2019-03-29 09:35:51 +0800

    201903290935

--- push.cmd
+++ push.cmd
@@ -8,15 +8,12 @@ nuget push "DawnxLite/bin/Release/DawnxLite.1.9.6.nupkg" -source nuget.org
 nuget push "~Library/Dawnx.AspNetCore/bin/Release/Dawnx.AspNetCore.1.9.6.nupkg" -source nuget.org
 nuget push "~Library/Dawnx.AspNetCore.IdentityUtility/bin/Release/Dawnx.AspNetCore.IdentityUtility.1.9.6.nupkg" -source nuget.org
 nuget push "~Library/Dawnx.AspNetCore.LiveAccountUtility/bin/Release/Dawnx.AspNetCore.LiveAccountUtility.1.9.6.nupkg" -source nuget.org
-::==== ~Dawnx.LuaEngine ===
+
+::==== ~Dawnx.Library ===
 nuget push "~Library/Dawnx.LuaEngine/bin/Release/Dawnx.LuaEngine.1.9.6.nupkg" -source nuget.org
-::==== ~Net ====
 nuget push "~Library/Dawnx.Net/bin/Release/Dawnx.Net.1.9.6.nupkg" -source nuget.org
-::==== ~NPOI ====
 nuget push "~Library/Dawnx.NPOI/bin/Release/Dawnx.NPOI.1.9.6.nupkg" -source nuget.org
-::==== ~Security ====
 nuget push "~Library/Dawnx.Security/bin/Release/Dawnx.Security.1.9.6.nupkg" -source nuget.org
-::==== ~Xml ====
 nuget push "~Library/Dawnx.Xml/bin/Release/Dawnx.Xml.1.9.6.nupkg" -source nuget.org
 
 ::==== ~Dawnx.Chinese ===

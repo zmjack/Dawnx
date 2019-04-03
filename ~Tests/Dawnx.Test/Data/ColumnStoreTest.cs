ede450a220378f1e0e9c11599b6c262e9935e352
zmjack <jack@zmland.com>
2019-04-04 00:40:28 +0800

    201904040039

--- ~Tests/Dawnx.Test/Data/ColumnStoreTest.cs
+++ ~Tests/Dawnx.Test/Data/ColumnStoreTest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -29,34 +30,14 @@ namespace Dawnx.Data.Test
 
         public bool ProxyLoaded { get; set; }
 
-        public static Registry Connect(IEnumerable<RegistryStore> columnStores)
+        public static Registry Connect(DbSet<RegistryStore> columnStores)
         {
             var proxy = new Registry().Proxy(new RegistryProxy<Registry, RegistryStore>());
             proxy.Load(columnStores);
             return proxy;
         }
     }
-
-    public class Registry2
-    {
-        [Key]
-        public Guid Id { get; set; }
-
-        public string Name
-        {
-            get => ColumnStores.FirstOrDefault(x => x.Key == "Name").Value;
-            set => ColumnStores.FirstOrDefault(x => x.Key == "Name").Value = value;
-        }
-
-        public string Sex
-        {
-            get => ColumnStores.FirstOrDefault(x => x.Key == "Sex").Value;
-            set => ColumnStores.FirstOrDefault(x => x.Key == "Sex").Value = value;
-        }
-
-        public IEnumerable<RegistryStore> ColumnStores { get; set; }
-    }
-
+    
     public class ColumnStoreTest
     {
         [Fact]
@@ -69,25 +50,6 @@ namespace Dawnx.Data.Test
             };
 
             var item = Registry.Connect(regs);
-            item.Name = "999";
-
-            Assert.Equal(Guid.Parse("0EA3F3B8-291A-4D23-A0DC-4BBC2F2BA9DB"), item.Id);
-            Assert.Equal("999", regs.First(x => x.Key == "Name").Value);
-        }
-
-        [Fact]
-        public void Test2()
-        {
-            var regs = new RegistryStore[]
-            {
-                new RegistryStore { EntityId = "0EA3F3B8-291A-4D23-A0DC-4BBC2F2BA9DB", Key = "Name", Value = "zmjack" },
-                new RegistryStore { EntityId = "0EA3F3B8-291A-4D23-A0DC-4BBC2F2BA9DB", Key = "Sex", Value = "Male" },
-            };
-
-            var item = new Registry2();
-            item.Id = Guid.Parse(regs[0].EntityId);
-            item.ColumnStores = regs;
-
             item.Name = "999";
 
             Assert.Equal(Guid.Parse("0EA3F3B8-291A-4D23-A0DC-4BBC2F2BA9DB"), item.Id);

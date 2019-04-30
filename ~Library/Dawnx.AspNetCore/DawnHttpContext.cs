fc5713138b0830aebaa06563b4d13f8bdcc7d65a
zmjack <jack@zmland.com>
2019-04-30 15:07:08 +0800

    201904301507

--- ~Library/Dawnx.AspNetCore/DawnHttpContext.cs
+++ ~Library/Dawnx.AspNetCore/DawnHttpContext.cs
@@ -22,19 +22,19 @@ namespace Dawnx.AspNetCore
         /// <returns></returns>
         public static string GetRefreshToken(this HttpContext @this) => @this.GetTokenAsync("refresh_token").Result;
 
-        public static void Login(this HttpContext @this, string userName, string[] roles = null)
-        {
-            @this.SignInAsync(new ClaimsPrincipal(new SimpleClaimsIdentity(userName, roles))).Wait();
-        }
-
+        public static void Login(this HttpContext @this, string userName, string[] roles = null) => LoginAsync(@this, userName, roles).Wait();
         public static async Task LoginAsync(this HttpContext @this, string userName, string[] roles = null)
-        {
-            await @this.SignInAsync(new ClaimsPrincipal(new SimpleClaimsIdentity(userName, roles)));
-        }
+            => await @this.SignInAsync(new ClaimsPrincipal(new SimpleClaimsIdentity(userName, roles)));
 
-        public static void Logout(this HttpContext @this) => @this.SignOutAsync().Wait();
+        public static void Login(this HttpContext @this, string scheme, string userName, string[] roles = null) => LoginAsync(@this, scheme, userName, roles).Wait();
+        public static async Task LoginAsync(this HttpContext @this, string scheme, string userName, string[] roles = null)
+            => await @this.SignInAsync(scheme, new ClaimsPrincipal(new SimpleClaimsIdentity(userName, roles)));
 
+        public static void Logout(this HttpContext @this) => LogoutAsync(@this).Wait();
         public static async Task LogoutAsync(this HttpContext @this) => await @this.SignOutAsync();
+
+        public static void Logout(this HttpContext @this, string shceme) => LogoutAsync(@this, shceme).Wait();
+        public static async Task LogoutAsync(this HttpContext @this, string shceme) => await @this.SignOutAsync(shceme);
 
     }
 }

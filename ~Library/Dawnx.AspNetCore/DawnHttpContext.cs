d1662af24ff21195e481dbe5e45cda14d607f115
zmjack <jack@zmland.com>
2019-04-30 19:29:48 +0800

    201940301929

--- ~Library/Dawnx.AspNetCore/DawnHttpContext.cs
+++ ~Library/Dawnx.AspNetCore/DawnHttpContext.cs
@@ -26,7 +26,7 @@ namespace Dawnx.AspNetCore
         public static void Login(this HttpContext @this, string userName, string[] roles = null) => LoginAsync(@this, userName, roles).Wait();
         public static async Task LoginAsync(this HttpContext @this, string userName, string[] roles = null)
             => await @this.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(
-                new SimpleClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme, userName, roles).ToClaimsIdentity()));
+                new SimpleClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme, userName, roles)));
 
         public static void Logout(this HttpContext @this) => LogoutAsync(@this).Wait();
         public static async Task LogoutAsync(this HttpContext @this) => await @this.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
@@ -34,7 +34,7 @@ namespace Dawnx.AspNetCore
         public static void Login(this HttpContext @this, string scheme, string userName, string[] roles = null) => LoginAsync(@this, scheme, userName, roles).Wait();
         public static async Task LoginAsync(this HttpContext @this, string scheme, string userName, string[] roles = null)
             => await @this.SignInAsync(scheme, new ClaimsPrincipal(
-                new SimpleClaimsIdentity(scheme, userName, roles).ToClaimsIdentity()));
+                new SimpleClaimsIdentity(scheme, userName, roles)));
 
         public static void Logout(this HttpContext @this, string shceme) => LogoutAsync(@this, shceme).Wait();
         public static async Task LogoutAsync(this HttpContext @this, string shceme) => await @this.SignOutAsync(shceme);

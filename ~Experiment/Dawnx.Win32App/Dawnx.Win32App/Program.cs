de1fdd124f6c2adfafebe8406e2321afb2532601
zmjack <jack@zmland.com>
2019-05-23 04:36:27 +0800

    201905230436

---
+++ ~Experiment/Dawnx.Win32App/Dawnx.Win32App/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using static Dawnx.Win32.NativeMethods;
+using static Dawnx.Win32.NativeConstants;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using Dawnx.Win32.PInvoke;
+
+namespace Dawnx.Win32App
+{
+    class Program
+    {
+        private static int[] pids;
+        static void Main(string[] args)
+        {
+            pids = Process.GetProcesses()
+                .Where(x => x.ProcessName.StartsWith("notepad"))
+                .Select(x => x.Id).ToArray();
+
+            EnumWindows(EnumWindowsProc, IntPtr.Zero);
+
+        }
+
+        protected static int EnumWindowsProc(IntPtr hwnd, IntPtr lParam)
+        {
+            using var pid = new AutoIntPtr<int>();
+
+            StringBuilder window_text_ptr = new StringBuilder(255);
+            string window_text;
+
+            GetWindowTextW(hwnd, window_text_ptr, 255);
+            window_text = window_text_ptr.ToString();
+
+            GetWindowThreadProcessId(hwnd, pid);
+
+            if (pids.Any(x => x == pid.Value))
+            {
+                if (window_text.StartsWith("无标题"))
+                {
+                    return 0;
+                }
+                else return 1;
+            }
+            else return 1;
+        }
+
+    }
+}
---
+++ ~NativeLibrary/Dawnx.Win32/PInvoke/MemoryAccessor.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dawnx.Win32
+{
+    //public partial class PInvoke
+    //{
+    //    public static bool ReadProcessMemory_(IntPtr hProcess, IntPtr lpBaseAddress, out int lpBuffer)
+    //    {
+    //        var nSize = (uint)sizeof(int);
+    //        var bytes = new byte[nSize];
+    //        uint lpNumberOfBytesRead;
+
+    //        bool ret = NativeMethods.ReadProcessMemory(hProcess, lpBaseAddress, bytes, nSize, out lpNumberOfBytesRead);
+    //        lpBuffer = BitConverter.ToInt32(bytes, 0);
+
+    //        return ret;
+    //    }
+    //    public static bool ReadProcessMemory_(IntPtr hProcess, IntPtr lpBaseAddress, out short lpBuffer)
+    //    {
+    //        var nSize = (uint)sizeof(short);
+    //        var bytes = new byte[nSize];
+    //        uint lpNumberOfBytesRead;
+
+    //        bool ret
+    //             = ReadProcessMemory(hProcess, lpBaseAddress, bytes, nSize, out lpNumberOfBytesRead);
+    //        lpBuffer = BitConverter.ToInt16(bytes, 0);
+
+    //        return ret;
+    //    }
+    //    public static bool ReadProcessMemory_(IntPtr hProcess, IntPtr lpBaseAddress, out float lpBuffer)
+    //    {
+    //        var nSize = (uint)sizeof(float);
+    //        var bytes = new byte[nSize];
+    //        uint lpNumberOfBytesRead;
+
+    //        bool ret
+    //             = ReadProcessMemory(hProcess, lpBaseAddress, bytes, nSize, out lpNumberOfBytesRead);
+    //        lpBuffer = BitConverter.ToSingle(bytes, 0);
+
+    //        return ret;
+    //    }
+    //    public static bool ReadProcessMemory_(IntPtr hProcess, IntPtr lpBaseAddress, out double lpBuffer)
+    //    {
+    //        var nSize = (uint)sizeof(double);
+    //        var bytes = new byte[nSize];
+    //        uint lpNumberOfBytesRead;
+
+    //        bool ret
+    //             = ReadProcessMemory(hProcess, lpBaseAddress, bytes, nSize, out lpNumberOfBytesRead);
+    //        lpBuffer = BitConverter.ToDouble(bytes, 0);
+
+    //        return ret;
+    //    }
+    //    public static bool ReadProcessMemory_(IntPtr hProcess, IntPtr lpBaseAddress, out byte lpBuffer)
+    //    {
+    //        var nSize = (uint)sizeof(char);
+    //        var bytes = new byte[nSize];
+    //        uint lpNumberOfBytesRead;
+
+    //        bool ret
+    //             = ReadProcessMemory(hProcess, lpBaseAddress, bytes, nSize, out lpNumberOfBytesRead);
+    //        lpBuffer = bytes[0];
+
+    //        return ret;
+    //    }
+    //    public static bool ReadProcessMemory_(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer)
+    //    {
+    //        var nSize = (uint)lpBuffer.Count();
+    //        var bytes = new byte[nSize];
+    //        uint lpNumberOfBytesRead;
+
+    //        bool ret
+    //             = ReadProcessMemory(hProcess, lpBaseAddress, bytes, nSize, out lpNumberOfBytesRead);
+    //        Array.Copy(bytes, lpBuffer, nSize);
+
+    //        return ret;
+    //    }
+    //}
+}

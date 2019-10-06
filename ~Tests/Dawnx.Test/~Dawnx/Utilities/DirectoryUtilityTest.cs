7ec989badce9fcaacaff5871c65605192e37651f
zmjack <jack@zmland.com>
2019-10-06 17:49:43 +0800

    201910061749

---
+++ ~Tests/Dawnx.Test/~Dawnx/Utilities/DirectoryUtilityTest.cs
@@ -0,0 +1,18 @@
+﻿using Dawnx.Utilities;
+using System;
+using System.Drawing;
+using Xunit;
+
+namespace Dawnx.Test.Utilities
+{
+    public class DirectoryUtilityTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(DirectoryUtility.IsDirectoryPath(@"\"));
+            Assert.True(DirectoryUtility.IsDirectoryPath(""));
+        }
+
+    }
+}

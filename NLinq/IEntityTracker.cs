243fa21eea527b8268edc7a70cd9da9658570731
zmjack <jack@zmland.com>
2019-10-27 18:27:07 +0800

    201910271827

--- NLinq/IEntityTracker.cs
+++ NLinq/IEntityTracker.cs
@@ -6,7 +6,7 @@ namespace NLinq
     public interface IEntityTracker
     {
         void OnInserting(DbContext context);
-        void OnUpdating(DbContext context, PropertyValues original);
+        void OnUpdating(DbContext context, PropertyValues origin);
         void OnDeleting(DbContext context);
     }
 

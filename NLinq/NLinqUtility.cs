56fa831622e65d84f0a6c0683ad9bbbc7d80909e
zmjack <jack@zmland.com>
2019-11-05 00:52:58 +0800

    201911050052

--- NLinq/NLinqUtility.cs
+++ NLinq/NLinqUtility.cs
@@ -1,9 +1,14 @@
 ﻿using Dawnx.Definition;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NLinq.ProviderFunctions;
 using NStandard;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 
@@ -73,12 +78,12 @@ namespace NLinq
         /// <summary>
         /// This method should be called before 'base.SaveChanges'.
         /// </summary>
-        /// <param name="this"></param>
+        /// <param name="context"></param>
         /// <param name="acceptAllChangesOnSuccess"></param>
         /// <returns></returns>
-        public static void IntelliTrack(DbContext @this, bool acceptAllChangesOnSuccess)
+        public static void IntelliTrack(DbContext context, bool acceptAllChangesOnSuccess)
         {
-            var entries = @this.ChangeTracker.Entries()
+            var entries = context.ChangeTracker.Entries()
                 .Where(x => new[] { EntityState.Added, EntityState.Modified, EntityState.Deleted }.Contains(x.State))
                 .ToArray();
 
@@ -110,11 +115,10 @@ namespace NLinq
                 if (entityType.IsImplement(typeof(IEntityTracker<,>)))
                 {
                     //TODO: Use TypeReflectionCacheContainer to optimize it in the futrue.
-                    var trackerType = typeof(IEntityTracker<,>).MakeGenericType(@this.GetType(), entityType);
+                    var trackerType = typeof(IEntityTracker<,>).MakeGenericType(context.GetType(), entityType);
                     var onInsertingMethod = trackerType.GetMethod(nameof(DefEntityTracker.OnInserting));
                     var onUpdatingMethod = trackerType.GetMethod(nameof(DefEntityTracker.OnUpdating));
                     var onDeletingMethod = trackerType.GetMethod(nameof(DefEntityTracker.OnDeleting));
-                    var onCompletingMethod = trackerType.GetMethod(nameof(DefEntityTracker.OnCompleting));
 
                     var origin = Activator.CreateInstance(entry.Entity.GetType());
                     foreach (var originValue in entry.OriginalValues.Properties)
@@ -122,13 +126,167 @@ namespace NLinq
 
                     switch (entry.State)
                     {
-                        case EntityState.Added: onInsertingMethod.Invoke(entity, new object[] { @this }); break;
-                        case EntityState.Modified: onUpdatingMethod.Invoke(entity, new object[] { @this, origin }); break;
-                        case EntityState.Deleted: onDeletingMethod.Invoke(entity, new object[] { @this }); break;
+                        case EntityState.Added: onInsertingMethod.Invoke(entity, new object[] { context }); break;
+                        case EntityState.Modified: onUpdatingMethod.Invoke(entity, new object[] { context, origin }); break;
+                        case EntityState.Deleted: onDeletingMethod.Invoke(entity, new object[] { context }); break;
                     }
-                    onCompletingMethod.Invoke(entity, new object[] { @this, entry.State });
                 }
+            }
 
+            CompleteTracker(context);
+        }
+
+        private static void CompleteTracker(DbContext context)
+        {
+            var entries = context.ChangeTracker.Entries()
+                .Where(x => new[] { EntityState.Added, EntityState.Modified, EntityState.Deleted }.Contains(x.State))
+                .Where(x => x.Entity.GetType().IsImplement(typeof(IEntityTracker<,>)))
+                .ToArray();
+
+            foreach (var entry in entries)
+            {
+                //TODO: Use TypeReflectionCacheContainer to optimize it in the futrue.
+                var entity = entry.Entity;
+                var entityType = entity.GetType();
+                var trackerType = typeof(IEntityTracker<,>).MakeGenericType(context.GetType(), entityType);
+                var onCompletingMethod = trackerType.GetMethod(nameof(DefEntityTracker.OnCompleting));
+                onCompletingMethod.Invoke(entity, new object[] { context, entry.State });
+            }
+        }
+
+        private static void ApplyCompositeKey(object entityTypeBuilder, Type modelClass)
+        {
+            var hasKeyMethod = entityTypeBuilder.GetType().GetMethod(nameof(EntityTypeBuilder.HasKey), new[] { typeof(string[]) });
+
+            var modelProps = modelClass.GetProperties()
+                .Where(x => x.GetCustomAttribute<CPKeyAttribute>() != null)
+                .OrderBy(x => x.GetCustomAttribute<CPKeyAttribute>().Order);
+            var propNames = modelProps.Select(x => x.Name).ToArray();
+
+            if (propNames.Any())
+                hasKeyMethod.Invoke(entityTypeBuilder, new object[] { propNames });
+        }
+
+        private static void ApplyIndexes(object entityTypeBuilder, Type modelClass)
+        {
+            var hasIndexMethod = entityTypeBuilder.GetType().GetMethod(nameof(EntityTypeBuilder.HasIndex), new[] { typeof(string[]) });
+            var propPairs = modelClass.GetProperties().Select(prop => new
+            {
+                Attribute = prop.GetCustomAttribute<IndexAttribute>(),
+                Property = prop,
+            }).Where(x => x.Attribute != null);
+
+            foreach (var pair in propPairs.Where(x => x.Attribute.Group == null))
+            {
+                var indexBuilder = hasIndexMethod.Invoke(entityTypeBuilder, new object[] { new[] { pair.Property.Name } }) as IndexBuilder;
+                if (pair.Attribute.Type == IndexType.Unique)
+                    indexBuilder.IsUnique();
+            }
+            foreach (var pairGroup in propPairs.Where(x => x.Attribute.Group != null).GroupBy(x => x.Attribute.Group))
+            {
+                var normalPairs = pairGroup.Where(x => x.Attribute.Type == IndexType.Normal);
+                if (normalPairs.Any())
+                    hasIndexMethod.Invoke(entityTypeBuilder, new object[] { normalPairs.Select(x => x.Property.Name).ToArray() });
+
+                var uniquePairs = pairGroup.Where(x => x.Attribute.Type == IndexType.Unique);
+                if (uniquePairs.Any())
+                {
+                    var indexBuilder = hasIndexMethod.Invoke(entityTypeBuilder, new object[] { uniquePairs.Select(x => x.Property.Name).ToArray() }) as IndexBuilder;
+                    indexBuilder.IsUnique();
+                }
+            }
+        }
+
+        private static void ApplyProviders(object entityTypeBuilder, Type modelClass)
+        {
+            var propertyMethod = entityTypeBuilder.GetType().GetMethodViaQualifiedName("Microsoft.EntityFrameworkCore.Metadata.Builders.PropertyBuilder Property(System.String)");
+
+            var modelProps = modelClass.GetProperties();
+            foreach (var modelProp in modelProps)
+            {
+                var attr = modelProp.GetCustomAttribute<ProviderAttribute>();
+                if (attr != null)
+                {
+                    var propertyBuilder = propertyMethod.Invoke(entityTypeBuilder, new object[] { modelProp.Name }) as PropertyBuilder;
+                    var hasConversionMethod = typeof(PropertyBuilder).GetMethod(nameof(PropertyBuilder.HasConversion), new[] { typeof(ValueConverter) });
+
+                    dynamic provider = Activator.CreateInstance(attr.ProviderType);
+                    hasConversionMethod.Invoke(propertyBuilder, new object[] { NLinqUtility.BuildConverter(provider) });
+                }
+            }
+        }
+
+        private static void ResolveTrackAttributes(EntityEntry entry, PropertyInfo[] properties)
+        {
+            var props_TrackCreationTime = properties.Where(x => x.IsMarkedAs<TrackCreationTimeAttribute>());
+            var props_TrackLastWrite = properties.Where(x => x.IsMarkedAs<TrackLastWriteTimeAttribute>());
+            var props_TrackLower = properties.Where(x => x.IsMarkedAs<TrackLowerAttribute>());
+            var props_TrackUpper = properties.Where(x => x.IsMarkedAs<TrackUpperAttribute>());
+            var props_TrackTrim = properties.Where(x => x.IsMarkedAs<TrackTrimAttribute>());
+            var props_TrackCondensed = properties.Where(x => x.IsMarkedAs<TrackCondensedAttribute>());
+            var props_Track = properties.Where(x => x.IsMarkedAs<TrackAttribute>());
+
+            var now = DateTime.Now;
+            switch (entry.State)
+            {
+                case EntityState.Added:
+                    SetPropertiesValue(props_TrackCreationTime, entry, v => now);
+                    SetPropertiesValue(props_TrackLastWrite, entry, v => now);
+                    SetPropertiesValue(props_TrackLower, entry, v => (v as string)?.ToLower());
+                    SetPropertiesValue(props_TrackUpper, entry, v => (v as string)?.ToUpper());
+                    SetPropertiesValue(props_TrackTrim, entry, v => (v as string)?.Trim());
+                    SetPropertiesValue(props_TrackCondensed, entry, v => ((v as string) ?? "").Unique());
+                    SetPropertiesValueForTrack(props_Track, entry);
+                    break;
+
+                case EntityState.Modified:
+                    SetPropertiesValue(props_TrackLastWrite, entry, v => now);
+                    SetPropertiesValue(props_TrackLower, entry, v => (v as string)?.ToLower());
+                    SetPropertiesValue(props_TrackUpper, entry, v => (v as string)?.ToUpper());
+                    SetPropertiesValue(props_TrackTrim, entry, v => (v as string)?.Trim());
+                    SetPropertiesValue(props_TrackCondensed, entry, v => (v as string)?.Unique());
+                    SetPropertiesValueForTrack(props_Track, entry);
+                    break;
+            }
+        }
+
+        private static void SetPropertiesValue(IEnumerable<PropertyInfo> properties, EntityEntry entry,
+            Func<object, object> evalMethod)
+        {
+            foreach (var prop in properties)
+            {
+                var oldValue = prop.GetValue(entry.Entity);
+                prop.SetValue(entry.Entity, evalMethod(oldValue));
+            }
+        }
+
+        private static void SetPropertiesValueForTrack(IEnumerable<PropertyInfo> properties, EntityEntry entry)
+        {
+            foreach (var prop in properties)
+            {
+                var trackAttr = prop.GetCustomAttribute<TrackAttribute>();
+
+                var type = trackAttr.Type;
+                var csharp = trackAttr.CSharp;
+                var entity = entry.Entity;
+                var entityType = entity.GetType()
+                    .For(self => self.Module.FullyQualifiedName != "<In Memory Module>" ? self : self.BaseType);
+
+                Script shell;
+                if (type != null)
+                {
+                    var references = new[] { type.Assembly.FullName };
+
+                    //If the invoked method is 'Method<T>(this T @this),
+                    //  the correct pattern is '@this.Method'
+                    shell = CSharpScript.Create($"using static {type.Namespace}.{type.Name};",
+                        ScriptOptions.Default.AddReferences(references), entityType)
+                        .ContinueWith(csharp);
+                }
+                else shell = CSharpScript.Create(csharp, ScriptOptions.Default, entityType);
+
+                var scriptState = shell.RunAsync(entity).Result;
+                prop.SetValue(entity, scriptState.ReturnValue);
             }
         }
 

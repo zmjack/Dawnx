cb083838cf0796864cf3b61894089287b6c990ba
zmjack <jack@zmland.com>
2019-11-11 02:58:26 +0800

    201911110258

--- ~Experiment/Dawnx.Xml.SampleWithHAP/Program.cs
+++ ~Experiment/Dawnx.Xml.SampleWithHAP/Program.cs
@@ -5,57 +5,57 @@ using System.Xml.XPath;
 
 namespace Dawnx.Xml.SampleWithHAP
 {
-class MyContext : XPathContext
-{
-    public MyContext() { }
-    public MyContext(string prefix) : base(prefix) { }
-
-    [XPathFunction("match")]
-    public bool RegexMatch1(string content, string regex)
+    class MyContext : XPathContext
     {
-        Console.WriteLine($"  * Invoke: {nameof(RegexMatch1)}");
-        return new Regex(regex).Match(content).Success;
+        public MyContext() { }
+        public MyContext(string prefix) : base(prefix) { }
+
+        [XPathFunction("match")]
+        public bool RegexMatch1(string content, string regex)
+        {
+            Console.WriteLine($"  * Invoke: {nameof(RegexMatch1)}");
+            return new Regex(regex).Match(content).Success;
+        }
+
+        [XPathFunction("match")]
+        public bool RegexMatch2(string regex, XPathNavigator docContext)
+        {
+            Console.WriteLine($"  * Invoke: {nameof(RegexMatch2)}");
+            return new Regex(regex).Match(docContext.InnerXml).Success;
+        }
     }
 
-    [XPathFunction("match")]
-    public bool RegexMatch2(string regex, XPathNavigator docContext)
+    class Program
     {
-        Console.WriteLine($"  * Invoke: {nameof(RegexMatch2)}");
-        return new Regex(regex).Match(docContext.InnerXml).Success;
-    }
-}
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        var html = @"
+        static void Main(string[] args)
+        {
+            var html = @"
 <div id=""info"">hello</div>
 <div class=""category1"">category_1</div>
 <div class=""category2"">category_2</div>
 <div id=""output"">bye</div>";
 
-        var doc = new HtmlDocument();
-        doc.LoadHtml(html);
-        var ctx = new MyContext("re");
+            var doc = new HtmlDocument();
+            doc.LoadHtml(html);
+            var ctx = new MyContext("re");
 
-        Console.WriteLine($"HTML:{html}");
-        Console.WriteLine();
+            Console.WriteLine($"HTML:{html}");
+            Console.WriteLine();
 
-        // Samples
-        var xpaths = new[]
-        {
+            // Samples
+            var xpaths = new[]
+            {
             @"//div[re:match(@class, 'category\d+')]",
             @"//div[re:match('(hello|bye)')]",
         };
-        foreach (var xpath in xpaths)
-        {
-            Console.WriteLine($"XPath: {xpath}");
-            var nodes = doc.DocumentNode.SelectNodes(ctx[xpath]);
-            foreach (var node in nodes)
-                Console.WriteLine(node.InnerHtml);
-            Console.WriteLine();
+            foreach (var xpath in xpaths)
+            {
+                Console.WriteLine($"XPath: {xpath}");
+                var nodes = doc.DocumentNode.SelectNodes(ctx[xpath]);
+                foreach (var node in nodes)
+                    Console.WriteLine(node.InnerHtml);
+                Console.WriteLine();
+            }
         }
     }
-}
 }
